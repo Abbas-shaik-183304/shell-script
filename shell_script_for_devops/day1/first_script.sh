3c790dba9b19e7d3dcc3c3fc0a3994cb4ccd874a
Abbas shaik <shaikabbas183304@gmail.com>
2024-10-15 12:43:10 +0530

    Update first_script.sh

--- shell_script_for_devops/day1/first_script.sh
+++ shell_script_for_devops/day1/first_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "hear the fear"
 echo " spencer: hey i m the johnson, you are searching for.."
 echo " killer: who said i m searching for you"
 echo " spencer: what?? then you dont wanna kill me"
